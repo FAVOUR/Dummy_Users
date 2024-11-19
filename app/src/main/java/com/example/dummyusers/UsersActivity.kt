69a4aafffb694f8181106410f21cd9cefd2e4437
FAVOUR <favouro@tractionapps.co>
2024-11-19 05:40:13 +0100

    Removed unnecessary files and renamed From MainActivity to UsersActivity

--- app/src/main/java/com/example/dummyusers/UsersActivity.kt
+++ app/src/main/java/com/example/dummyusers/UsersActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dummyusers.ui.theme.DummyUsersTheme
 
-class MainActivity : ComponentActivity() {
+class UsersActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
